b06783d723adc9b8a8372322a36731624fb7d04d
Michelle Moon <robbins.michelle@ymail.com>
2021-02-26 16:27:51 -0600

    promise

--- closure.js
+++ closure.js
@@ -1,12 +1,14 @@
 // A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function’s scope from an inner function. In JavaScript, closures are created every time a function is created, at function creation time.
 
 function makeFunc() {
-    var name = 'Mozilla';
+    var name = 'Friend';
     function displayName() {
-      alert(name);
+      alert("Hi" + " " + name);
     }
     return displayName;
   }
   
   var myFunc = makeFunc();
   myFunc();
+
+//   Essentially, we have a function within another function called as the return
---
+++ promise.js
@@ -0,0 +1,7 @@
+// The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+// Benefits include escaping callback hell 
+const promiseA = new Promise( (resolutionFunc,rejectionFunc) => {
+    resolutionFunc(777);
+});
+promiseA.then( (val) => console.log("asynchronous logging has val:",val) );
+console.log("immediate logging");
