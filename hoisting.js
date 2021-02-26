b7e843453388a82ecb34b6e4633f49f2d0509dba
Michelle Moon <robbins.michelle@ymail.com>
2021-02-26 15:44:43 -0600

    redo hoisting

--- hoisting.js
+++ hoisting.js
@@ -1,16 +1,18 @@
 // Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of their scope before code execution. 
 // Inevitably, this means that no matter where functions and variables are declared they are moved to the top of their scope regardless of whether their scope is global or local.
 
+var x; // x is global
 function example(){
-    var x = 5;
+    x = 5;
     document.getElementById("demo").innerHTML = x;
 }
-
 // VS 
 
-var x;
-x = 5;
-
-function example(){
+function example(){ 
+    x = 5;
     document.getElementById("demo").innerHTML = x;
+    var x; // x can be created after it's use;
 }
+
+carName = "Volvo";
+const carName; // This will result in a syntax error and not run
